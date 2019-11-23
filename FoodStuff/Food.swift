969139fffe21a5cbdeb6e3ae6524b56b14ff1f55
sebastianchoo <sebastian_choo>
2019-11-23 13:51:10 +0800

    Added Food struct

---
+++ FoodStuff/Food.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  FoodStuff
+//
+//  Created by Sebastian Choo on 23/11/19.
+//  Copyright © 2019 Aakash Sanjay Mehta. All rights reserved.
+//
+
+import Foundation
+struct Food {
+    var name: String
+    var expiryDate: Date
+    var storageInfo: String
+    
+    static func dateToSec(day: Int) -> Int{
+        let seconds = day*24*60*60
+        return seconds
+    }
+}
