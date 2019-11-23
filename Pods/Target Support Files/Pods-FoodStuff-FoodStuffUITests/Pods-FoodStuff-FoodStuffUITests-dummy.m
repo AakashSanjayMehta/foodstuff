dc9b94e71f8b27549212c1e067bdfbdbafe4bbe7
AakashSanjayMehta <asrm003@gmail.com>
2019-11-23 11:19:26 +0800

    Firebase added

---
+++ Pods/Target Support Files/Pods-FoodStuff-FoodStuffUITests/Pods-FoodStuff-FoodStuffUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodStuff_FoodStuffUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodStuff_FoodStuffUITests
+@end
---
+++ Pods/Target Support Files/Pods-FoodStuffTests/Pods-FoodStuffTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodStuffTests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodStuffTests
+@end
